ba74ec78730d764871dd91f306a2c30d39d8153f
imready1947 <imready1947@yahoo.com>
2023-01-20 18:32:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-rxjs-forkjoin-example-Rajesh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-rxjs-forkjoin-example-ne96lv)
---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { forkJoin, zip } from 'rxjs';
+
+interface User {
+  id: number;
+}
+
+interface Post {
+  id: number;
+}
+
+@Component({
+  selector: 'my-app',
+  template: ` 
+    RXJS EXAMPLE. See Console
+    <div *ngIf="data">
+      <pre>First User email: {{data[0][0].email}}</pre>
+      <pre>First Post title: {{data[1][0].title | json}}</pre>
+    </div>
+  `,
+})
+export class AppComponent {
+  USERS = 'https://jsonplaceholder.typicode.com/users/';
+  POSTS = 'https://jsonplaceholder.typicode.com/posts/';
+  data: [User[], Post[]];
+  constructor(http: HttpClient) {
+    const users = http.get<User[]>(this.USERS);
+    const posts = http.get<Post[]>(this.POSTS);
+
+    forkJoin([users, posts]).subscribe((res) => {
+      this.data = res;
+      console.log('User and Post', res);
+    });
+  }
+}
